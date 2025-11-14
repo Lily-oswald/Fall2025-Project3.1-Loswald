6a772a72f69db1e3d1f96804dd23a48f4b2dcb9a
Lily-oswald <Lilyoswald2005@gmail.com>
2025-11-13 18:30:15 -0600

    added actor

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -1,20 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Fall2025_Project3._1_Loswald.Models
+public class Actor
 {
-    public class Actor
-    {
-        [Key] // Primary key property
-        public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? IMDB_link { get; set; }
+    public string? Gender { get; set; }
+    public int Age { get; set; }
+    public int ID { get; set; }
 
-        [Required]
-        public string Name { get; set; } = string.Empty;
-
-        public string? Gender { get; set; }
-
-        public int Age { get; set; }
-
-        [Display(Name = "IMDB Link")]
-        public string? IMDB_link { get; set; }
-    }
 }
